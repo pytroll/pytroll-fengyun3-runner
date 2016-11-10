0d9a4ab1caff86c12cfdafcdaa0ff4f24578445b
Panu Lahtinen <pnuu+git@iki.fi>
2016-11-10 10:59:34 +0200

    Copy from Trollduction to remove huge dependency

---
+++ modis_runner/helper_functions.py
@@ -0,0 +1,12 @@
+import netifaces
+
+
+def get_local_ips():
+    inet_addrs = [netifaces.ifaddresses(iface).get(netifaces.AF_INET)
+                  for iface in netifaces.interfaces()]
+    ips = []
+    for addr in inet_addrs:
+        if addr is not None:
+            for add in addr:
+                ips.append(add['addr'])
+    return ips
